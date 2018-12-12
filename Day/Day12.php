95239b3f5bb3388526a6186ea8857a55e283ace6
Stephen <git@galatea.nl>
2018-12-12 09:08:36 +0100

    day 12 #2

--- Day/Day12.php
+++ Day/Day12.php
@@ -8,81 +8,25 @@
 
 namespace Advent\Y2018\Day;
 
-use Advent\Y2018\Helper\PotRule;
-
 class Day12 extends AbstractDayProblem
 {
     private $start = '';
     private $rules = [];
 
 
-    private $minPot=0;
-
-    private function addPots($state) {
-   //     echo $state.PHP_EOL;
-        $org = strlen($state);
-        $state = '...'.ltrim($state, '.');
-        $this->minPot -= (strlen($state) - $org);
-
-    //    echo $state."[$org] ".strlen($state)." $this->minPot\n";
-
-        $state = rtrim($state, '.').'...';
-        return $state;
-
-    }
-
+    private $minPot = 0;
 
     public function solve(array $input)
     {
         if (!$this->rules) {
             $this->parseInput($input);
         }
-        $state = $this->start;
-        $state= $this->addPots($state);
-        $minPot = 0;
-        for ($generation = 1; $generation <= 20; $generation++) {
-            $nextState = '..';
-            //add empty pots in front
-            for ($x = $minPot; $x < strlen($state) - 2; $x++) {
-                $situation = substr($state, $x, 5);
-                $result = $this->rules[$situation] ?? '.';
 
-                $nextState .= $result;
-            }
-            //echo ($generation - 1)."\t".$state."\n";
-           // $nextState = str_repeat('.', abs($minPot)).$state;
-            $nextState=$this->addPots($nextState);
-            $state = $nextState;
-
-           // echo "\t".$nextState." [$this->minPot]\n\n\n";
-        }
-
-
-    ///    echo $state.PHP_EOL;
-        $org = strlen($state);
-        $state = '...'.ltrim($state, '.');
-        $minPot -= (strlen($state) - $org);
-
-    //    echo $state."[$org] ".strlen($state)." $minPot\n";
-
-        $pot = $minPot;
-        $total = 0;
-        foreach (str_split($state) as $num => $char) {
-            if ($char === '#') {
-             //   echo ($num + $this->minPot)."\n";
-                $total += $num + $this->minPot;
-                $pot++;
-            }
-        }
-
-        return $total;
-        echo $state."\n";
-        echo $minPot."\n";
+        return $this->generate(20);
     }
 
     public function parseInput(array $input)
     {
-        print_r($input);
         $start = array_shift($input);
         $this->start = trim(substr($start, strpos($start, ':') + 1));
         foreach ($input as $line) {
@@ -92,11 +36,68 @@ class Day12 extends AbstractDayProblem
             }
             $this->rules [substr($line, 0, 5)] = substr($line, -1);
         }
+    }
 
+    private function generate(int $generations)
+    {
+
+        $this->minPot = 0;
+        $state = $this->start;
+        $state = $this->addPots($state);
+        $prevCount = $this->calcTotal($state);
+        for ($generation = 1; $generation <= $generations; $generation++) {
+            $nextState = '';
+            //add empty pots in front
+            for ($x = 0; $x < strlen($state); $x++) {
+                $situation = substr($state, $x, 5);
+                $result = $this->rules[$situation] ?? '.';
+
+                $nextState .= $result;
+            }
+            $nextState = $this->addPots('..'.$nextState);
+            $newCount = $this->calcTotal($nextState);
+            if ($state === $nextState) {
+                //state is the same, so it hasn't changed and won't change the next generations.
+                //they will only move in the pots, so calc diff for this round and multiply by next rounds
+                $diff = $newCount - $prevCount;
+
+                return ($diff * ($generations - $generation)) + $newCount;
+            }
+            $prevCount = $newCount;
+            $state = $nextState;
+        }
+
+        return $prevCount;
+    }
+
+    private function addPots($state)
+    {
+        //     echo $state.PHP_EOL;
+        $org = strlen($state);
+        $state = '...'.ltrim($state, '.');
+        $this->minPot -= (strlen($state) - $org);
+
+        //    echo $state."[$org] ".strlen($state)." $this->minPot\n";
+
+        $state = rtrim($state, '.').'...';
+
+        return $state;
+    }
+
+    private function calcTotal($state)
+    {
+        $total = 0;
+        foreach (str_split($state) as $num => $char) {
+            if ($char === '#') {
+                $total += $num + $this->minPot;
+            }
+        }
+
+        return $total;
     }
 
     public function solve2(array $input)
     {
-        // TODO: Implement solve2() method.
+        return $this->generate(50000000000);
     }
 }
